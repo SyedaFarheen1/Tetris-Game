3a5415d04b0f18bd8985a04757222629b86d5e61
Manal Muneer <manalmuneer24@gmail.com>
2025-05-05 23:26:14 +0500

    Board.h files

--- OOP-Project-Tetris/Board.cpp
+++ OOP-Project-Tetris/Board.cpp
@@ -1,2 +1,10 @@
 #include "Board.h"
 #include "Piece.h"
+
+Board::Board() {
+   
+    for (int i = 0; i < ROWS; ++i)
+        for (int j = 0; j < COLS; ++j)
+            grid[i][j] = 0;  // Initialize grid to 0 (empty)
+}
+
--- OOP-Project-Tetris/Board.h
+++ OOP-Project-Tetris/Board.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
+const int ROWS = 20;
+const int COLS = 10;
+const int BLOCK_SIZE = 30; 
+
+class Board {
+private:
+    int grid[ROWS][COLS];
+    sf::RectangleShape cell;
+
+public:
+    Board();
+    void draw(sf::RenderWindow& window);
+    void setBlock(int row, int col, sf::Color color); 
+};

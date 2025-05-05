625ba64dd55634c1425c5e45a710acf3d6aeb6a8
Manal Muneer <manalmuneer24@gmail.com>
2025-05-05 23:42:14 +0500

    Updated board.h

--- OOP-Project-Tetris/Board.h
+++ OOP-Project-Tetris/Board.h
@@ -1,17 +1,11 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-const int ROWS = 20;
-const int COLS = 10;
-const int BLOCK_SIZE = 30; 
-
 class Board {
 private:
-    int grid[ROWS][COLS];
-    sf::RectangleShape cell;
+    sf::RectangleShape boardOutline;
 
 public:
     Board();
     void draw(sf::RenderWindow& window);
-    void setBlock(int row, int col, sf::Color color); 
 };

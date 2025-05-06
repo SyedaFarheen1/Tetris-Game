d2177b843e7fd50389381c3e6c0510998871e4ea
Manal Muneer <manalmuneer24@gmail.com>
2025-05-06 16:12:08 +0500

    Added include files in main

--- OOP-Project-Tetris/Board.cpp
+++ OOP-Project-Tetris/Board.cpp
@@ -1,10 +1,21 @@
 #include "Board.h"
 #include "Piece.h"
 
+const int ROWS = 20;
+const int COLS = 10;
+const int BLOCK_SIZE = 30;
+
 Board::Board() {
-   
-    for (int i = 0; i < ROWS; ++i)
-        for (int j = 0; j < COLS; ++j)
-            grid[i][j] = 0;  // Initialize grid to 0 (empty)
+    int width = COLS * BLOCK_SIZE;
+    int height = ROWS * BLOCK_SIZE;
+
+    boardOutline.setSize(sf::Vector2f(width - 2, height - 2)); // -2 for visible border
+    boardOutline.setPosition(1, 1); // slight offset
+    boardOutline.setFillColor(sf::Color::Transparent);
+    boardOutline.setOutlineThickness(2);
+    boardOutline.setOutlineColor(sf::Color::White);
 }
 
+void Board::draw(sf::RenderWindow& window) {
+    window.draw(boardOutline);
+}
--- OOP-Project-Tetris/main.cpp
+++ OOP-Project-Tetris/main.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include "Game.h"
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+#include <array>
+
 using namespace std;
+
+
+int main() {
+
+
+
+	return 0;
+}
